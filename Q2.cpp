c2f0761cc1fd9b20cf341a319f6053fb1b141a91
Vanisha2494 <22103287@mail.jiit.ac.in>
2024-08-10 10:50:57 +0530

    Create Q2.cpp
    Write a program (C/C++) that takes an integer input from the user and finds all prime numbers
    up to that integer.

---
+++ Q2.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector <int> prime(int x,vector <int>&ans)
+{
+    //vector <int> ans;
+    for(int i=2 ; i<x; i++)
+    {
+       
+            int fac=0;
+            for(int j=2;j<i;j++)
+            {
+                if(i%j==0)
+                fac++;
+            }
+            if(fac==0)
+            ans.push_back(i);
+           
+       
+    }
+    return ans;
+}
+
+int main()
+{
+   
+    cout<<"Enter the integer upto which you want all prime numbers: ";
+    int x;
+    cin>>x;
+    vector <int>ans; prime(x,ans);
+   
+    for(int i=0; i<ans.size();i++)
+    {
+        cout<<ans[i]<<" ";
+    }    
+   
+}
